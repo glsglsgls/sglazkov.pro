7d705a086971bd0198c4cc70aa006a1b9ad4af23
s.glazkov <s.glazkov@polymatica.ru>
2023-06-21 18:40:00 +0300

    fix metrics

--- front/src/js/header_footer.js
+++ front/src/js/header_footer.js
@@ -12,25 +12,6 @@ const head_part = `<title>Сергей Глазков - личный сайт</t
 <link rel="stylesheet" href="css/books.css">`;
 
 
-const yametrika = `<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(93944403, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-    });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/93944403" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-`;
-
-
 const header = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
     <div class="burger-menu">
@@ -86,7 +67,6 @@ const scripts = [
 const body = document.querySelector('body');
 const head = document.querySelector('head');
 head.innerHTML += head_part;
-head.innerHTML += yametrika;
 
 let head_el = document.createElement('header');
 head_el.classList.add('head');
