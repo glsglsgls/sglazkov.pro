0b9fe52e59ec309276b81edf3d3e79d0d653d19f
s.glazkov <sergey.glazkov@axenix.pro>
2023-08-02 01:29:07 +0300

    fix animations

--- front/src/js/frog.js
+++ front/src/js/frog.js
@@ -1,40 +1,100 @@
 const frogText = `
 <div class="fg">
-    <button class="close_btn">
-        <div class="close_btn_text" href="#" onclick="stopShow();return false;"">X</div>
+    <button class="close_btn" onclick="stopShow();return false;"">
+        <div class="close_btn_text" href="#">X</div>
     </button>
-    <img class="frog_img" src="img/toad.gif">
 </div>
 `;
 document.querySelector('body').innerHTML += frogText;
+// <img class="frog_img slide-in-top" src="img/toad.gif"></img>
 
 const frog = document.querySelector('.frog_img');
 const back = document.querySelector('.fg');
 var music = new Audio('frog.mp3');
+let intervalLoop = setTimeout(() => 2, 1320);
+var timeouts = [];
 
 
-function showToad(){
-    back.style.display = 'block';
-    setTimeout(function(){frog.style.opacity = '100%';}, 1700);
-    frog.setAttribute("src", 'img/toad.gif');
-    setTimeout(function(){frog.setAttribute("src", 'img/toad2.gif');}, 12000);
+
+function __getOneToad(toad_el){
+    toad_el.classList.add('frog_img', 'slide-in-top');
+    toad_el.setAttribute("src", 'img/toad.gif');
+    back.prepend(toad_el);
+    timeouts.push(setTimeout(() => toad_el.classList.add('slide-out-left'), 470));
+    timeouts.push(setTimeout(() => toad_el.remove(), 6000));
 }
+
+function getOneToad(){
+    __getOneToad(document.createElement('img'));
+}
+
+function getManyToads(){
+    getOneToad();
+    timeouts.push(setTimeout(function(){getManyToads();}, 1320));
+}
+
+
+
+function getAnotherOneToad(){
+    let toad_el = document.createElement('img')
+    toad_el.classList.add('frog_img', 'fade-in');
+    toad_el.setAttribute("src", 'img/toad2.gif');
+    back.prepend(toad_el);
+    
+    timeouts.push(setTimeout(function(){
+        toad_el.classList.remove('fade-in');
+        toad_el.classList.add('vibrate-1');
+    }, 505));
+    timeouts.push(setTimeout(() => getBothToads(toad_el), 10800));
+}
+
+function getManyAnotherToads(){
+    getAnotherOneToad();
+}
+
+function getBothToads(another_toad){
+    hideToad();
+}
+
+
 
 function hideToad(){
-    frog.style.opacity = '0%';
-    back.style.display= 'none';
+    clearAllTimeouts();
+    document.querySelectorAll('.frog_img').forEach(function(el){el.remove()});
 }
 
+
+
+function changeToad(){
+    hideToad();
+    getManyAnotherToads();
+}
+
+
+function startShow(){
+    back.style.display = 'block';
+    timeouts.push(setTimeout(() => getManyToads(), 1300));
+    timeouts.push(setTimeout(() => changeToad(), 12000));
+}
+
+
+
 function playMusic(){
-    setTimeout(function(){document.querySelector('.fg').style.display= 'flex';}, 100)
-    showToad();
+    timeouts.push(setTimeout(function(){document.querySelector('.fg').style.display= 'flex';}, 100));
+    startShow();
     music.play();
-    music.onended = function() {hideToad();};
+    music.onended = () => stopShow();
 }
 
 function stopShow(){
     hideToad();
     music.pause();
     music.currentTime = 0;
+    back.style.display= 'none';
 }
 
+function clearAllTimeouts(){
+    for (var i=0; i<timeouts.length; i++) {
+        clearTimeout(timeouts[i]);
+    }
+}
